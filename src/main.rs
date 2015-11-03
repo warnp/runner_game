f6bcb4b30852466ee763ca9ac80cd5a8bd3b153a
unknown <rassie@LFR009891.corp.capgemini.com>
2015-11-03 16:16:18 +0100

    Add jump control over sprite

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,42 @@ fn generate_sprite(display: &glium::backend::glutin_backend::GlutinFacade, sprit
 
 }
 
+fn jump_function(sp: &mut [vertex::Vertex], jump: &mut bool, touch_ground: &mut bool, jump_height: f32, time_between: f32) {
+
+    if *jump && *touch_ground {
+
+        *touch_ground = false;
+        sp[0].position[1] = jump_height + sp[0].position[1];
+        sp[1].position[1] = jump_height + sp[1].position[1];
+        sp[2].position[1] = jump_height + sp[2].position[1];
+        sp[3].position[1] = jump_height + sp[3].position[1];
+
+    } else {
+        if sp[2].position[1] > -0.5 {
+            *touch_ground = false;
+            *jump = false;
+            sp[0].position[1] = sp[0].position[1] - 0.3 * time_between;
+            sp[1].position[1] = sp[1].position[1] - 0.3 * time_between;
+            sp[2].position[1] = sp[2].position[1] - 0.3 * time_between;
+            sp[3].position[1] = sp[3].position[1] - 0.3 * time_between;
+        } else {
+            *touch_ground = true;
+
+        }
+    }
+}
+
+fn move_to_left(sp: &mut [vertex::Vertex], time_between: f32){
+    sp[0].position[0] = sp[0].position[0] - 0.5 * time_between;
+    sp[1].position[0] = sp[1].position[0] - 0.5 * time_between;
+    sp[2].position[0] = sp[2].position[0] - 0.5 * time_between;
+    sp[3].position[0] = sp[3].position[0] - 0.5 * time_between;
+}
+
+
 fn main() {
+
+
     let screen_height = 600.0;
     let screen_width = 800.0;
     let mut show_fps = false;
@@ -50,9 +85,8 @@ fn main() {
     let vert = vec![Sprite::new(0.0,0.0,[1.0,0.0,0.0,1.0],0),
                     Sprite::new(0.5,0.0,[1.0,0.0,0.0,1.0],1)];
 
-    // println!("{:?}", vert);
 
-    let mut shaders = shader_manager::Shaders::new(vec![&include_bytes!("../content/NatureForests.png")[..],&include_bytes!("../content/11532.png")[..]]);
+    let mut shaders = shader_manager::Shaders::new(vec![&include_bytes!("../content/VFKM2.png")[..],&include_bytes!("../content/11532.png")[..]]);
     shaders.compile_shaders(&display);
 
     let program = shaders.get_compiled_shader("simple_shader");
@@ -63,13 +97,17 @@ fn main() {
 
     let mut vertex_buffer = sprite_manager.get_vertex_buffer(&display);
     let indices = sprite_manager.get_index_buffer(&display).unwrap();
-    // let img = v.set_image().unwrap();
+
+
+
+    let mut go_up = false;
+    let jump_height = 0.5;
+    let mut jump = false;
+    let mut touch_ground = false;
 
 
     let mut t : f32 = 0.0;
-    // let mut time : f32 = time::precise_time_ns() as f32;
     let mut old_time = 0.0;
-
     let mut horizontal_position = 0.0;
     loop{
         let mut target = display.draw();
@@ -83,7 +121,7 @@ fn main() {
         if t > 60.0{
             time_between = time/1000000000.0 - old_time/1000000000.0;
         }
-        // println!("{:?}", time_between);
+
         let fps = 1.0/(time_between );
         if show_fps {
             println!("FPS : {}", fps);
@@ -92,17 +130,26 @@ fn main() {
 
         {
             let mut mapping = vertex_buffer.map();
-
+            let mut index = 0;
             for sp in mapping.chunks_mut(4){
                 // sp[0].position[0] = 0.01 * time_between + sp[0].position[0];
                 // sp[1].position[0] = 0.01 * time_between + sp[1].position[0];
                 // sp[2].position[0] = 0.01 * time_between + sp[2].position[0];
                 // sp[3].position[0] = 0.01 * time_between + sp[3].position[0];
 
-                sp[0].position[0] = horizontal_position + sp[0].position[0];
-                sp[1].position[0] = horizontal_position + sp[1].position[0];
-                sp[2].position[0] = horizontal_position + sp[2].position[0];
-                sp[3].position[0] = horizontal_position + sp[3].position[0];
+                if index == 0 {
+                    jump_function(sp, &mut jump, &mut touch_ground, jump_height, time_between);
+
+                    if t % 10.0 == 0.0 {
+                        sp[0].tex_coords[0] = 0.0625 + sp[0].tex_coords[0];
+                        sp[1].tex_coords[0] = 0.0625 + sp[1].tex_coords[0];
+                        sp[2].tex_coords[0] = 0.0625 + sp[2].tex_coords[0];
+                        sp[3].tex_coords[0] = 0.0625 + sp[3].tex_coords[0];
+                    }
+                }
+                else {
+                    move_to_left(sp, time_between);
+                }
 
                 //
                 // if t >= 60.0 && t % 10.0 == 0.0 {
@@ -118,9 +165,8 @@ fn main() {
                 //         sp[3].tex_coords[0] = (-0.0977) * 5.0 + sp[3].tex_coords[0];
                 //     }
                 // }
-
+                index = index + 1;
             }
-
 
         }
 
@@ -143,9 +189,9 @@ fn main() {
         for ev in display.poll_events(){
             println!("{:?}", ev);
             match ev {
-                glium::glutin::Event::KeyboardInput(glium::glutin::ElementState::Pressed, 1, Some(Space)) => horizontal_position = 0.5,
-                glium::glutin::Event::KeyboardInput(glium::glutin::ElementState::Released, 1, Some(Space)) => horizontal_position = 0.0,
-                glium::glutin::Event::KeyboardInput(glium::glutin::ElementState::Released,1,Some(Escape)) => return,
+                glium::glutin::Event::KeyboardInput(glium::glutin::ElementState::Pressed, _, Some(glium::glutin::VirtualKeyCode::Space)) => jump = true,
+                glium::glutin::Event::KeyboardInput(glium::glutin::ElementState::Released, _, Some(glium::glutin::VirtualKeyCode::Space)) => horizontal_position = 0.0,
+                glium::glutin::Event::KeyboardInput(glium::glutin::ElementState::Released,_,Some(glium::glutin::VirtualKeyCode::Escape)) => return,
                 glium::glutin::Event::Closed => return,
                 _ => ()
             }
